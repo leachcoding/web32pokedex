fc3d94b175f0b9627b08af325c9dc16d8003f558
Jay Leach <leachcoding@gmail.com>
2020-06-10 19:50:46 -0400

    Started pokedex, pokemon list rendered, pagination built, 6/10/20

---
+++ pokedex-app/src/components/Pokemon/Pokemon.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Pokemon = props => {
+  let idVal = '';
+
+  if(props.data.id < 10) {
+    idVal = 'Id#: 00' + props.data.id;
+  } else if(props.data.id < 100) {
+    idVal = 'Id#: 0' + props.data.id;
+  } else {
+    idVal = 'Id#: ' + props.data.id;
+  }
+
+  const capitalize = (str) => {
+
+    if(typeof str !== 'string') {
+      return ''
+    }
+
+
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+  return (
+    <div className='card'>
+      <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.data.id}.png`}/>
+      <p>{idVal}</p>
+      <p>{capitalize(props.data.name)}</p>
+    </div>
+  )
+}
+
+export default Pokemon;
---
+++ pokedex-app/src/components/PokemonList/PokemonList.js
@@ -0,0 +1,60 @@
+import React, {useState,useEffect} from 'react';
+import axios from 'axios';
+// We will probably later need another component to import here
+import Pokemon from '../Pokemon/Pokemon.js';
+
+function PokemonList() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [pages, setPages] = useState(0);
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon/?offset=${pages}&limit=20/`)
+        .then(res => {
+          console.log(res.data.results);
+          let pokemonValue = res.data.results;
+          pokemonValue.map(pokemon => {
+            let regexPoke = /\/pokemon\/(\d+)\//;
+            let id = pokemon.url.match(regexPoke)[1];
+            return (pokemon['id'] = id)
+          })
+          setData(pokemonValue);
+          setLoading(false);
+        })
+        .catch(err => {
+          console.log('error', err);
+        });
+    };
+
+    fetchData();
+  }, [pages]);
+
+  console.log(data);
+  if(loading) return <p>I'm Loading! Please be patient</p>;
+
+  return (
+    <>
+    <div>
+      <div className='cards'>
+        <div className = 'paginationRow'>
+          <button onClick={() => setPages(pages -100)}>Prev - 5</button>
+          <button onClick={() => setPages(pages -20)}>Prev</button>
+          <button onClick={() => setPages(pages + 20)}>Next</button>
+          <button onClick={() => setPages(pages + 100)}>Next + 5</button>
+        </div>
+       {data.map(pokemon => <Pokemon data={pokemon} key={pokemon.name}/>)}
+        <div className = 'paginationRow'>
+          <button onClick={() => setPages(pages -100)}>Prev - 5</button>
+          <button onClick={() => setPages(pages -20)}>Prev</button>
+          <button onClick={() => setPages(pages + 20)}>Next</button>
+          <button onClick={() => setPages(pages + 100)}>Next + 5</button>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default PokemonList;
